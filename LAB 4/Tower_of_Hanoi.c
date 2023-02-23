30fdec0f0aed2b6ef230bd730ddf592bbb08190a
Grimmjow-Jaegerjaquez <109174297+Grimmjow-Jaegerjaquez@users.noreply.github.com>
2023-02-23 08:49:57 +0530

    Add files via upload

---
+++ LAB 4/Tower_of_Hanoi.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+void toh(int n, char from_rod, char to_rod, char aux_rod){
+    if(n == 0)
+        return;
+
+    toh(n - 1,from_rod,to_rod,aux_rod);
+    printf("Move disk %d from rod %c to rod %c \n",n,from_rod,to_rod);
+    toh(n - 1,from_rod,to_rod,aux_rod);
+}
+
+int main(){
+    int n = 3;
+    //printf("Enter number of disks : ");
+    //scanf("%d",&n);
+
+    toh(3,'A','C','B');
+    return 0;
+}
